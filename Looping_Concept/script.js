09a334f361def9c723c536b43d63018b4a065e60
Masha-Menaga <rameast.mass@gmail.com>
2024-09-28 20:26:18 +0530

    Looping Concepts With JS

---
+++ Looping_Concept/script.js
@@ -0,0 +1,21 @@
+// for loop
+let n = 5;
+for (let i = 1; i <= n; i++) {
+  console.log("Hello " + i);
+}
+
+//while Loop
+let a = 10;
+let b = 1;
+while (b <= a) {
+  console.log("Hi " + b);
+  b++;
+}
+
+//do-while loop
+let x = 15;
+let y = 0;
+do {
+  console.log("It's Me " + y);
+  y++;
+} while (y <= x);
